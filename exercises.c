b2a9dcc8cf1974a61952dc66ebd5494a13a8acbc
Your Name <you@example.com>
2024-03-15 23:04:33 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -47,15 +47,13 @@ los números pares del arreglo original.
 */
 int *filterEvenNumbers(int arr[], int size, int *newSize)
 {
-  int *newArray = NULL;
+  int *newArray = (int *)malloc(sizeof(int) * size);
   (*newSize) = 0;
 
   for (int i = 0 ; i < size ; i++)
   {
     if (arr[i] % 2 == 0)
     {
-      newArray = (int *)realloc(newArray, sizeof(int) * (*newSize + 1));
-
       newArray[*newSize] = arr[i];
       (*newSize)++;
     }
